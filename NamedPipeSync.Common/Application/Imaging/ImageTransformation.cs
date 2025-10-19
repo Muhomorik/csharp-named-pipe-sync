4a8ec094026a0887225ae27755b14649aed4912c
Muhomorik <Muhomorik@users.noreply.github.com>
2025-10-19 20:43:28 +0200

    Add `ImageTransformation` enum with `Sepia` option for image processing enhancements

---
+++ NamedPipeSync.Common/Application/Imaging/ImageTransformation.cs
@@ -0,0 +1,12 @@
+﻿namespace NamedPipeSync.Common.Application.Imaging;
+
+/// <summary>
+/// Enumerates supported image transformations.
+/// </summary>
+public enum ImageTransformation
+{
+    /// <summary>
+    /// Apply a Sepia tone effect to the image.
+    /// </summary>
+    Sepia = 0,
+}
